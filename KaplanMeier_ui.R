336a886a7ae37a10066b97ce824a50058cd4291d
Coralie Williams <coraliewilliams@outlook.com>
2019-02-27 10:43:44 +0100

    Update KaplanMeier_ui.R

--- KaplanMeier_ui.R
+++ KaplanMeier_ui.R
@@ -2,11 +2,12 @@
 install.packages("shiny"); install.packages("ggplot2"); install.packages("dplyr")
 install.packages("survminer"); install.packages("survival")
 
-# Install packages
+# Load library
 library(shiny);library(ggplot2);library(dplyr);library(survminer);library(survival); library(httr)
 
 # Load data
-baz3 <- read.csv2("https://raw.githubusercontent.com/opetchey/RREEBES/master/Beninca_etal_2008_Nature/data/nutrients_original.csv",sep = ",")
+### check data source
+### baz3 <- read.csv2("https://raw.githubusercontent.com/opetchey/RREEBES/master/Beninca_etal_2008_Nature/data/nutrients_original.csv",sep = ",")
 baz4 <- baz3[baz3$date==baz3$timepoint+baz3$date.bl,]
 baz5 <- baz4 %>%
   select("SLC5A7_p.I89V_MODERATE","chgt", "timepoint","PTGENDER","SIGMAR1_p.Q2P_MODERATE","ABCA7_p.G215S_MODERATE","bl")
